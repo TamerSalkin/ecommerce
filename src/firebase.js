d7b9af0c3de7f14fa8c33e6d6b22921f117173fe
Tamer <tsalkin@gmail.com>
2022-05-04 21:34:58 -0500

    project setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA_GdV0u3tSzlvPa3KHvWLOzLEXn24Nkag",
+  authDomain: "e-comm-as-ts.firebaseapp.com",
+  projectId: "e-comm-as-ts",
+  storageBucket: "e-comm-as-ts.appspot.com",
+  messagingSenderId: "99742340814",
+  appId: "1:99742340814:web:65134218fe6036962de29f",
+  measurementId: "G-243BLNJ3QF",
+};
