52395a7e7e784c8bd4d6d2f13e07e8367b183012
Tamer <tsalkin@gmail.com>
2022-05-06 12:57:31 -0500

    home page added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import "./styles/app.scss";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Checkout from "./components/Checkout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  // BEM
+  // BEM convention. We use two underscore in classnames for this.
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/checkout" element={<Checkout />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
